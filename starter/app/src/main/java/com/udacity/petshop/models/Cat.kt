91f321c6e03e7d4e6bd979b21c1bd7d11b98cd8e
soleilvi <soleil.vivero@gmail.com>
2022-07-30 19:00:02 -0700

    Changed the cat data class

--- starter/app/src/main/java/com/udacity/petshop/models/Cat.kt
+++ starter/app/src/main/java/com/udacity/petshop/models/Cat.kt
@@ -4,5 +4,5 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Cat(var name: String, var size: Double, var company: String, var description: String,
-               val images: List<String> = mutableListOf()) : Parcelable
+data class Cat(var name: String, var avgWeight: Double, var furPattern: String,
+               var description: String, val images: List<String> = mutableListOf()) : Parcelable
