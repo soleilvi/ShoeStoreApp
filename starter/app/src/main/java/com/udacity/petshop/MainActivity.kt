d0649c3566d53b443105fe84ef6825fa549ce674
soleilvi <soleil.vivero@gmail.com>
2022-08-04 16:18:24 -0700

    Got AppBarConfiguration working (but my screen is now blue)

--- starter/app/src/main/java/com/udacity/petshop/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/petshop/MainActivity.kt
@@ -4,23 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import petshop.R
 import petshop.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         // Timber setup
         Timber.plant(Timber.DebugTree())
 
+        // Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         // Set up Action Bar
+        setSupportActionBar(binding.toolbar)
         val navController = this.findNavController(R.id.myNavHostFragment)
-        // NavigationUI.setupActionBarWithNavController(this, navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
